2a13608a1714e2de905db2457f83fb5b7f5eee0a
eclipse_user <eclipse@126.com>
2019-03-29 21:38:46 +0800

    hot_fix修改

--- src/main/java/aaa/Happy.java
+++ src/main/java/aaa/Happy.java
@@ -5,5 +5,8 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("right2 ...");
 		System.out.println("left1 .....");
+		
+		//hot_fixÐÞ¸Ä
+		System.out.println("hot_fix");
 	}
 }
