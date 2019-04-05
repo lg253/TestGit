e80c782587ca43f081dbfd5ebcd893172926f22a
lip <lip@126.com>
2019-04-05 08:26:04 +0800

    hot_fix提交

--- src/main/java/aaa/getTest_git2.java
+++ src/main/java/aaa/getTest_git2.java
@@ -1,1 +1,1 @@
-package aaa;public class getTest_git2 {    // psvm    public static void main(String[] args) {        // sout        System.out.println("update_gitmaster");    }}
+package aaa;public class getTest_git2 {    // psvm    public static void main(String[] args) {        // sout        System.out.println("update_gitmaster-hotfixupdate");    }}
